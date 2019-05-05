1c63d79c32a4c1f4eb0895549cecb566b5090c5a
HoleLin <HoleLin@163.com>
2019-05-05 17:20:53 +0800

    add 原型模式Code

---
+++ src/main/java/design/pattern/creational/prototype/MailUtil.java
@@ -0,0 +1,23 @@
+package design.pattern.creational.prototype;
+
+import java.text.MessageFormat;
+
+/**
+ * ClassName: MailUtil
+ *
+ * @author HoleLin
+ * @version 1.0
+ * @date 2019/5/5
+ */
+
+public class MailUtil {
+	public static void sendMail(Mail mail) {
+		String outputContent = "向{0}同学,邮件地址:{1},邮件内容:{2},发送邮件";
+		System.out.println(MessageFormat.format(outputContent, mail.getName(),
+				mail.getEmailAddress(), mail.getContent()));
+	}
+
+	public static void saveOriginMailRecord(Mail mail) {
+		System.out.println("存储originMail记录,originMail: " + mail.getContent());
+	}
+}
---
+++ src/main/java/design/pattern/creational/prototype/abstractprototype/A.java
@@ -0,0 +1,9 @@
+package design.pattern.creational.prototype.abstractprototype;
+
+
+public abstract class A implements Cloneable{
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
